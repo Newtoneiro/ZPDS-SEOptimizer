c20693cadab3c29348bb9401a067237dda5a1617
mkrakows <01158769@pw.edu.pl>
2025-01-21 20:33:06 +0100

    gen

--- lib/article_generation.py
+++ lib/article_generation.py
@@ -1,17 +1,27 @@
 import ollama
 
+class Article_Specification:
+    def __init__(self, title, keywords, length, tone):
+        self.title = title
+        self.keywords = keywords
+        self.length = length
+        self.tone = tone
 
-def generate_article(prompt):
+    def generate(self):
+        self.content = generate_article(self.title)
+        return self.content
+
+def generate_article(article_specifications: Article_Specification):
+    content = f"Generate a SEO inner content article with the following specifications:\n\nTitle: {article_specifications.title}\nKeywords: {', '.join(article_specifications.keywords)}\nLength: {article_specifications.length} words\nTone: {article_specifications.tone}"
     response = ollama.chat(
         model="llama3.2",
-        messages=[
-        {"role": "user", "content": prompt}
-        ]
+         messages=[
+                {"role": "assistant", "content": content}
+            ]
     )
     print(response.message.content)
-
     return response.message.content
     
+article = Article_Specification("DUPA TITLE DUPA TUTLE", ["joke", "LLM models"], 10000, "funny")
 
-
-generate_article("Tell me a joke about LLM models")
+generate_article(article)
