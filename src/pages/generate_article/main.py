db049044032b40dcebeb65c5c646893d0bfa5d33
Newtoneiro <latosek.bar@gmail.com>
2025-01-22 17:20:35 +0100

    integrate model

--- src/pages/generate_article/main.py
+++ src/pages/generate_article/main.py
@@ -1,29 +1,6 @@
 import streamlit as st
-import ollama
-from streamlit_extras.stylable_container import stylable_container
 from streamlit_tags import st_tags
-
-
-class Article_Specification:
-    def __init__(self, title, keywords, length, tone):
-        self.title = title
-        self.keywords = keywords
-        self.length = length
-        self.tone = tone
-
-    def generate_prompt(self):
-        prompt = (
-            f"I want you to answer in the role of a very talender copywriter that specializes in article generation."
-            f"I want you to output ONLY the contents of the article, no comments, no disclaimers - pure text."
-            f"Generate a high-quality SEO article with the following specifications:\n\n"
-            f"Title: {self.title}\n"
-            f"Primary Keywords: {', '.join(self.keywords)}\n"
-            f"Target Word Count: {self.length}\n"
-            f"Tone of Voice: {self.tone}\n\n"
-            f"Please ensure the article is well-structured, engaging, and includes relevant subheadings. Use keywords naturally and provide valuable insights to readers."
-            f"Please translate the output to polish."
-        )
-        return " ".join(prompt)
+from src.utils import generate_article
 
 
 def render_page():
@@ -31,7 +8,7 @@ def render_page():
 
     keywords = st_tags(
         label="Dodaj słowa kluczowe",
-        text="Wciśnij enter, żeby dodać kolejne słowo kluczowe",
+        text="Wciśnij enter, żeby dodać",
     )
 
     topic = st.text_input(
@@ -64,24 +41,14 @@ def render_page():
         else:
             with st.spinner("Trwają obliczenia..."):
                 article_text = generate_article(
-                    Article_Specification(title=topic, keywords=keywords, tone=tone, length=length)
+                    title=topic,
+                    keywords=keywords,
+                    tone=tone,
+                    length=length
                 )
             show_article(article_text)
 
 
-def generate_article(article_specifications: Article_Specification):
-    content = article_specifications.generate_prompt()
-    response = ollama.chat(
-        model="llama3.2",
-        messages=[
-            {"role": "assistant", "content": content}
-        ]
-    )
-    return response.message.content
-
-
 def show_article(article_text: str):
-    with stylable_container(
-        key="container_with_border",
-    ):
+    with st.container(border=True):
         st.write(article_text)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+import ollama
 
 
 STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static')
@@ -11,3 +12,46 @@ def get_svg_path(name):
     if os.path.exists(svg_path):
         return svg_path
     return None
+
+
+class ArticleSpecification:
+    def __init__(self, title, keywords, length, tone):
+        self.title = title
+        self.keywords = keywords
+        self.length = length
+        self.tone = tone
+
+    def generate_prompt(self):
+        prompt = (
+            "I want you to answer in the role of a very talender copywriter that specializes in article generation."
+            "I want you to output ONLY the contents of the article, no comments, no disclaimers - pure text."
+            "Generate a high-quality SEO article with the following specifications:\n\n"
+            f"Title: {self.title}\n"
+            f"Primary Keywords: {', '.join(self.keywords)}\n"
+            f"Target Word Count: {self.length}\n"
+            f"Tone of Voice: {self.tone}\n\n"
+            "Please ensure the article is well-structured, engaging, and includes relevant subheadings."
+            "Use keywords naturally and provide valuable insights to readers."
+            "Please translate the output to polish."
+        )
+        return " ".join(prompt)
+
+
+def generate_article(title, keywords, tone, length):
+    prompt = ArticleSpecification(
+        title=title,
+        keywords=keywords,
+        tone=tone,
+        length=length
+    ).generate_prompt()
+
+    response = ollama.chat(
+        model="mwiewior/bielik",
+        messages=[
+            {"role": "system", "content": "Jesteś ekspertem w tworzeniu treści SEO."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    article_content = response.message.content
+    return article_content
